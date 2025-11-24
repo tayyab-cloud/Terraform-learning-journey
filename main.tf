a4ef6ef539260a6c33ad83164bfbe206b5691dfa
tayyab-cloud <https://github.com/tayyab-cloud>
2025-11-24 22:46:19 +0500

    feat: provision initial AWS EC2 instance using Terraform

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-east-1"
+    
+  
+}
+resource "aws_instance" "this" {
+    ami = "ami-0fa3fe0fa7920f68e"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "MyFirstInstance"
+    }
+  
+}
