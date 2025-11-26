4eac3a40c7d7a4749b174ac15188aa5e17855626
tayyab-cloud <https://github.com/tayyab-cloud>
2025-11-27 02:03:04 +0500

    Fix backend: remove duplicate key and close terraform block

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "tayyab-terraform-state-unique-2025"
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
   }
     
   
