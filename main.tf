58952e1d33dfc09f5191cb76c38925f159b74b57
tayyab-cloud <https://github.com/tayyab-cloud>
2025-12-01 01:46:23 +0500

    feat(vault): integrate HashiCorp Vault with Terraform using AppRole auth
    - Configured Vault provider with AppRole authentication
    - Added data source to fetch KV secrets dynamically
    - Created EC2 instance with tags displaying fetched secrets for verification

--- main.tf
+++ main.tf
@@ -1,14 +1,55 @@
 provider "aws" {
-  region = "us-east-1"
-}
-
-
-
-module "instance_type" {
-  source        = "./module/ec2"
-  instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
-  ami_id        = var.ami_ubuntu
   
 }
 
 
+terraform {
+  required_providers {
+    vault = {
+      source = "hashicorp/vault"
+      version = "3.25.0" # Ya jo latest ho
+    }
+  }
+}
+
+provider "vault" {
+  # Yahan apni EC2 ki PUBLIC IP likho
+  address = "http://35.175.182.69:8200"
+  
+  skip_child_token = true
+
+  auth_login {
+    path = "auth/approle/login"
+
+    parameters = {
+      # Jo Notepad me copy kia tha wo yahan paste karo
+      role_id   = "6c2a57eb-471a-a74c-ead1-bac5e06fd74a"
+      secret_id = "d122c32f-be7c-ee08-9f3b-88210568ee7e"
+    }
+  }
+}
+
+# Ye block Vault se Secret FETCH karega (Read karega)
+data "vault_kv_secret_v2" "example" {
+  mount = "kv"           # Jo humne browser me engine banaya tha
+  name  = "test-secret"  # Secret ka naam
+}
+
+resource "aws_instance" "this" {
+  ami           = var.ami_ubuntu
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Vault-EC2-Instance"
+    FetchedSecret     = data.vault_kv_secret_v2.example.data["username"]
+  }
+
+  
+}
+
+# Test karne ke liye ke secret aya ya nahi, hum output use karenge
+output "secret_value" {
+  # Ye sensitive true isliye hai taake terminal pe password direct na dikhe
+  value     = data.vault_kv_secret_v2.example.data["username"]
+  sensitive = true
+}
